6db79d5b88d69f3e6987069d58a55b4878494672
suria003 <suryadr1111@gmail.com>
2025-10-16 12:24:24 +0530

    server hosting set-up && origin set-up && api versio set-up

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -15,28 +15,41 @@ const { segmentsRouter } = require("./routes/SegmentsRouter");
 const app = express();
 const port = 3000;
 
+const allowedOrigins = [
+    "https://woo-commerce-project.web.app"
+];
+
 // MIDDLEWARE
-app.use(CORS());
+app.use(CORS({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true,
+}));
 app.use(express.json());
 
 // ROUTER'S
 app.use("/api/v0.1/authendication", authenRouter);
-app.use("/session", sessionRouter);
-app.use("/ingests", ingestsProduct);
-app.use("/segments", segmentsRouter);
+app.use("/api/v0.1/session", sessionRouter);
+app.use("/api/v0.1//ingests", ingestsProduct);
+app.use("/api/v0.1/segments", segmentsRouter);
 
 app.get('/', async (req, res) => {
     res.send("Hello, welcome to Woo Commerce.")
 });
 
 // PRODUCT DATA'S
-app.get('/products', async (req, res) => {
-    try{
+app.get('/api/v0.1/products', async (req, res) => {
+    try {
         const [productDatas] = await connections.execute('SELECT * FROM products');
-        
+
         return res.json(productDatas);
 
-    } catch(error) {
+    } catch (error) {
         return res.status(500).json({
             status: 500,
             message: error.message
@@ -45,7 +58,7 @@ app.get('/products', async (req, res) => {
 });
 
 // START SERVER
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server running at http://localhost:${port}`);
 });
 
